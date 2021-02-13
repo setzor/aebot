52e7ca0c51a06e1083a4e7889cc6b32ddfdf060c
George Stevenson <stevenson.gt@gmail.com>
2021-02-13 14:21:54 +0200

    WIP2 - add repeater/echo

--- setbot.py
+++ setbot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher, requests, io
 import logging
 
 logging.basicConfig(format='%(levelname)s - %(message)s',
@@ -6,20 +6,39 @@ logging.basicConfig(format='%(levelname)s - %(message)s',
 logger = logging.getLogger(__name__)
 updater = None
 
+def echo(update, context):
+    command = context.args[0].lower()
+    if("on" == command):
+        context.user_data[echo] = True
+        update.message.reply_text("Repeater Started")
+    elif("off" == command):
+        context.user_data[echo] = False
+        update.message.reply_text("Repeater Stopped")
+
 def start(update, context):
     sss = "Hi there, I am AE. A little test bot."
     update.message.reply_text(sss)
 
+def repeater(update, context):
+    if context.user_data[echo]:
+        update.message.reply_text(update.message.text)
+
 def start_bot():
     global updater
     updater = Updater(
-        '1643323711:AAH9fIbNx3R2Tl764yyrT_OSNGwXfA64FzE', use_context=True)
+        'xxx', use_context=True)
     
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('echo', echo))
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text, repeater))
+
+    
+    
 
     updater.start_polling()
     updater.idle()
+    
 start_bot()
 
 
